b9ab1f33a2f466aa6fef94cc288c36b25a83e6f6
Dan <122179463+dekker-git@users.noreply.github.com>
2023-01-26 08:53:39 -0700

    initialized querySelectors

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const progress = document.getElementById('progress');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+const circles = document.querySelectorAll('.circle');
